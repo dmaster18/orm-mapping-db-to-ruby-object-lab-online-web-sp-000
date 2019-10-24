4b853859ac7431bebe75654d0461095f41c5d01a
David Strauch <dmaster18@gmail.com>
2019-10-24 20:01:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = "SELECT*FROM students WHERE grade = 10 ORDER BY id ASC"
-    DB[:conn].execute(sql).map {|row|
+    DB[:conn].execute(sql, grade).map {|row|
       self.new_from_db(row)
     }
   end
